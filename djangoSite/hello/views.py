d66b4415a6fee64c1ce82c9617fb6c2b460f3a57
sebastus <divingclimber@gmail.com>
2014-11-14 11:10:23 -0800

    add some comments, remove unnecessary code

--- djangoSite/hello/views.py
+++ djangoSite/hello/views.py
@@ -6,17 +6,28 @@ from hello.oauth import oauth_code
 def index(request):
 	template = loader.get_template('hello/step1.html')
 	if request.method == 'GET':
+	
+		# see if we're coming back from the redirect to authenticate
 		aad_code = request.GET.get('code','')
-		context = {'url': '', 'status_code':'', 'history':'', 'aad_code':aad_code}
 		if aad_code != '':
+			context = {'aad_code':aad_code}
 			oauth_obj = oauth_code()
 			resp = oauth_obj.get_subscriptions(aad_code)
 			context['subscriptions'] = resp.text
 			context['requestheaders'] = resp.request.headers
+			
+		# if not coming back from redirect, just render
 		return render(request, 'hello/step1.html', context)
+		
 	elif request.method == 'POST':
+	
+		# POST as a result of clicking the LogIn submit button
 		oauth_obj = oauth_code()
+		
+		# do the outreach to https://login.windows.net/common/oauth2/authorize
 		oauth_obj.do_common()
-		context = {'url': oauth_obj.url, 'status_code':oauth_obj.status_code, 'history':oauth_obj.history, 'aad_code':''}
+		
+		# redirect to the URL passed back by that service call - this is the authentication page @ AAD
+		# the redirect_uri brings us right back here (above) but this time with the 'code' query parameter
 		return redirect(oauth_obj.url)
 	
